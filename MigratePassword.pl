7592af7be9e36092c4dc82e48462076b1af94170
pristinestoned <37197210+pristinestoned@users.noreply.github.com>
2026-02-12 13:20:08 -0500

    Added fix to run with sudo
    This file must be run with sudo

---
+++ MigratePassword.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+# 
+# Code written by Brian Ponnampalam brian2004 [at] hotmail.com
+#
+# A program to retrive active users from passwd file that are using bash shell
+
+use strict;
+
+my $pwdfile="/etc/passwd";
+my $workpassfile="./workpasswd";
+my @shadfile=`cat /etc/shadow`;
+
+open (INFO,$pwdfile) or die ("Could not open file");
+open (PASS,'>',$workpassfile) or die ("Could not open file");
+
+while(my $line=<INFO>) {
+   
+   #print $line;
+   my @users = split(/:/,$line); 
+   
+   if ($line =~ /\/bin\/bash/) {
+      print "Printing hash for: ".$users[0]."\n";
+      
+      foreach my $passline(@shadfile) {
+         #print $_;
+	 my @passhash = split(/:/,$passline); 
+	 if($passline =~ /$users[0]/) {
+	    #print $passhash[0].":".$passhash[1]."\n"; 
+	    $line =~ s/:x:/:$passhash[1]:/g;
+	    print PASS $line;
+         }
+      } 
+   }
+}
+close(PASS);
+close(INFO);
